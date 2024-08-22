b97b2b7612723215eb8a1e309be5bef9ac0555ed
MattBartie <mattbartie3@gmail.com>
2024-08-22 15:34:02 -0400

    Initial Commit

---
+++ Quick Screen Journal/Quick_Screen_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_Screen_JournalApp.swift
+//  Quick Screen Journal
+//
+//  Created by Matt Bartie on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Quick_Screen_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
